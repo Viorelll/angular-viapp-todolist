102e742c4c01b0e05d89e7cf5298f08164945fe5
Viorelll <llleroiv@gmail.com>
2020-07-31 17:02:49 +0300

    1 file modified

--- src/app/pages/todo/todo.component.ts
+++ src/app/pages/todo/todo.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { TodoService } from '../../services';
 
 @Component({
   selector: 'to-do',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.scss']
+  styleUrls: ['./todo.component.scss'],
+  providers: [TodoService]
 })
 
 export class TodoComponent {
  title = 'app works!';
+
+ constructor(private todoService: TodoService) {
+ }
+
+ toggleTodoComplete(todo) {
+   //this.todoService.
+ }
 }
