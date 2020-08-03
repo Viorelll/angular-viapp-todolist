9a9adc30b03d70fa347e027b75363241aa80d39b
Viorelll <llleroiv@gmail.com>
2020-08-03 09:59:34 +0300

    2 files modified

--- src/app/pages/todo/todo.module.ts
+++ src/app/pages/todo/todo.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { TodoComponent } from "./todo.component";
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forChild([{
       path: '',
       component: TodoComponent
